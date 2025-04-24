50985f3a34161f489663c9bc7062f33a3bb4c7d0
naresh <surepallinaresh143@gmail.com>
2025-04-24 22:25:56 +0530

    store and carts functionality

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
